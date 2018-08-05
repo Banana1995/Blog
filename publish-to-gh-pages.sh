44ca8a3a433f687b917413edf65145de32ae09e6
banana1995 <1770261049@qq.com>
2018-08-05 14:10:54 +0800

    自动构建提交脚本

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -11,15 +11,15 @@ mv .deploy_git/.git/ ./public/
 
 cd ./public
 
-git config user.name "Leafney"
-git config user.email "babycoolzx@126.com"
+git config user.name "Banana1995"
+git config user.email "1770261049@qq.com"
 
 # add commit timestamp
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 
 # Github Pages
-git push --force --quiet "https://${TravisCIToken}@${GH_REF}" master:master
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
 
 # Coding Pages
-git push --force --quiet "https://Leafney:${CodingToken}@${CD_REF}" master:master
+# git push --force --quiet "https://Leafney:${CodingToken}@${CD_REF}" master:master
